3a96b68f30fb977bf249af52150a16d4606d2f48
Chris Smith <42@chrisryansmith.com>
2017-04-18 09:42:25 -0600

    finish iteration 1: pantry has stock, can restock and report quantities

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -7,4 +7,9 @@ class Pantry
   def stock_check(item_name)
     @stock[item_name] || 0
   end
+
+  def restock(item, quantity)
+    current = @stock[item] || 0
+    @stock[item] = current + quantity
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,18 +4,31 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
   def pantry_exists
-    @pantry = Pantry.new
+    panty = Pantry.new
   end
 
   def test_pantry_has_empty_stock
-    @pantry = Pantry.new
-    assert @pantry.stock.empty?
-    assert_instance_of Hash, @pantry.stock
+    panty = Pantry.new
+    assert panty.stock.empty?
+    assert_instance_of Hash, panty.stock
   end
 
   def test_pantry_can_check_for_an_item_and_returns_0_when_not_found
-    @pantry = Pantry.new
-    assert_equal 0, @pantry.stock_check("Cheese")
+    panty = Pantry.new
+    assert_equal 0, panty.stock_check("Cheese")
+  end
+
+  def test_pantry_can_add_stock_and_return_quantity
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+
+  def test_pantry_can_add_stock_to_existing_item
+    pantry = Pantry.new
+    pantry.restock("Cheese", 10)
+    pantry.restock("Cheese", 20)
+    assert_equal 30, pantry.stock_check("Cheese")
   end
 end
 
