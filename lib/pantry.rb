b022f896a51bacd6ad0cd9ff084d88e5d052711a
Chris Smith <42@chrisryansmith.com>
2017-04-18 09:37:36 -0600

    start pantry with stock checking

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,10 @@
 class Pantry
+  attr_reader :stock
+  def initialize
+    @stock = {}
+  end
 
+  def stock_check(item_name)
+    @stock[item_name] || 0
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,32 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def pantry_exists
+    @pantry = Pantry.new
+  end
 
+  def test_pantry_has_empty_stock
+    @pantry = Pantry.new
+    assert @pantry.stock.empty?
+    assert_instance_of Hash, @pantry.stock
+  end
+
+  def test_pantry_can_check_for_an_item_and_returns_0_when_not_found
+    @pantry = Pantry.new
+    assert_equal 0, @pantry.stock_check("Cheese")
+  end
 end
+
+
+
+# pantry.stock_check("Cheese")
+# # => 0
+#
+# pantry.restock("Cheese", 10)
+# pantry.stock_check("Cheese")
+# # => 10
+#
+# pantry.restock("Cheese", 20)
+# pantry.stock_check("Cheese")
+# # => 30
+# ```
