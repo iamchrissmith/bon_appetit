96d2a3aeb532cc04cfc09f3ead2b11ee4bb6bd93
Chris Smith <42@chrisryansmith.com>
2017-04-18 10:23:51 -0600

    add initial what_can_i_make

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source "https://rubygems.org"
 gem "minitest"
 gem "pry"
 gem "rake"
+gem 'pry-state'
+gem 'pry-byebug'
--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,8 +1,10 @@
+require 'pry'
 class Pantry
   attr_reader :stock, :shopping_list
   def initialize
     @stock = {}
     @shopping_list = {}
+    @cookbook = {}
   end
 
   def stock_check(item_name)
@@ -24,5 +26,22 @@ class Pantry
     end
     puts list
     list.join("\n")
+  end
+
+  def add_to_cookbook(recipe)
+    @cookbook[recipe.name] = recipe.ingredients
+  end
+
+  def what_can_i_make
+    available_food = find_available_recipes
+    available_food.map {|recipes| recipes.first}
+  end
+
+  def find_available_recipes
+    @cookbook.find_all do |recipe, ingredients|
+      ingredients.all? do |name, amount_needed|
+        amount_needed <= stock[name]
+      end
+    end
   end
 end
